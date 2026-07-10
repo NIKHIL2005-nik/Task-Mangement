b2664aa3d74017df1e5a83ca15bed630012299e0
Nikhil Pundir <nikhilpundir736@gmail.com>
2026-07-10 19:51:11 +0530

    add middleware for cookie parsing and CORS; implement ApiError, ApiResponse, and asyncHandler utilities

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import express from "express"
+import cookieParser from "cookie-parser"
+import cors from 'cors'
+
+const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true // to allow cookies and the authorisation headers.
+}))
+app.use(cookieParser())
+
+app.use(express.json({limit: '16kb'})) // for the data in the express we send in json format and to parse it in the req.body, without this middleware the data is not parsed in the req.body
+app.use(express.urlencoded({
+    extended: true,
+    limit: '16kb'
+}))
+app.use(express.static('public'))
+
+
+export {app}
---
+++ src/utils/ApiError.js
@@ -0,0 +1,12 @@
+class ApiError extends Error {
+    constructor(statuscode,
+        message = "Something went wrong",
+        errors = []
+    ){
+        super(message)
+        this.statuscode = statuscode
+        this.errors = errors
+    }
+}
+
+export {ApiError}
